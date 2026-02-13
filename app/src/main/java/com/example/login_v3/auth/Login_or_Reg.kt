2b948c321955b801ecf9bc062779893d5c8dc71e
Eric85710 <ericchen87887@gmailcom>
2026-02-13 20:33:51 +0800

    add startup button

--- app/src/main/java/com/example/login_v3/auth/Login_or_Reg.kt
+++ app/src/main/java/com/example/login_v3/auth/Login_or_Reg.kt
@@ -1,14 +1,24 @@
 package com.example.login_v3.auth
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Login_or_Reg_page(
@@ -27,5 +37,35 @@ fun Login_or_Reg_page(
                 )
             )
             .padding(paddingValues),
-    ) { }
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        Text(
+            text = "Welcome",
+            fontSize = 40.sp,      // 字體大小（像標題）
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+
+
+
+        Row() {
+            Button(
+                onClick = {}
+            ) {
+                Text( text = "Login")
+            }
+
+            Button(
+                onClick = {}
+            ) {
+                Text( text = "Register")
+            }
+        }
+
+
+
+    }
 }
