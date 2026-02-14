d8a0abb6261c049effaead5d78750ba0b71500be
Eric85710 <ericchen87887@gmailcom>
2026-02-14 21:06:00 +0800

    reg_V1

--- app/src/main/java/com/example/login_v3/auth/Reg_Page.kt
+++ app/src/main/java/com/example/login_v3/auth/Reg_Page.kt
@@ -1,11 +1,17 @@
 package com.example.login_v3.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -15,6 +21,18 @@ fun Register_Screen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFDA7029),
+                        Color(0xFF777777),
+                        Color(0xFFB34800)
+                    )
+                )
+            )
+            .padding(paddingValues),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = "Reg",
