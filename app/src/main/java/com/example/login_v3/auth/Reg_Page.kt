66c89b2ec81e23ba47da1218b0ec3abe1c9f35ed
Eric85710 <ericchen87887@gmailcom>
2026-02-14 21:14:33 +0800

    reg_V2

--- app/src/main/java/com/example/login_v3/auth/Reg_Page.kt
+++ app/src/main/java/com/example/login_v3/auth/Reg_Page.kt
@@ -1,17 +1,26 @@
 package com.example.login_v3.auth
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -34,10 +43,46 @@ fun Register_Screen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Reg",
-            fontSize = 30.sp
+
+
+        Text(text = "Register",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
         )
+
+        //Reg_box
+        Box(
+            modifier = Modifier
+                .size(360.dp)
+                .padding(8.dp),
+            contentAlignment = Alignment.Center
+        ){
+
+            //glass effect
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .clip(RoundedCornerShape(16.dp)) // 🔥 一定要有
+                    .blur(
+                        radius = 10.dp,
+                        edgeTreatment = BlurredEdgeTreatment.Unbounded
+                    )
+                    .background(
+                        Color.White.copy(alpha = 0.18f)
+                    )
+                    .border( // ⭐ 玻璃邊框
+                        width = 1.dp,
+                        color = Color.White.copy(alpha = 0.35f),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+            )
+
+        }
+
+
+
+
     }
 
 }
