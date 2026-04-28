840c611f5462d6e59c35c5d324eb061edfecc3e7
Eric85710 <ericchen87887@gmail.com>
2026-04-28 16:38:32 +0800

    auth to server-V8

--- app/src/main/java/com/example/login_v3/data/api/api_class/AuthRequest_Response.kt
+++ app/src/main/java/com/example/login_v3/data/api/api_class/AuthRequest_Response.kt
@@ -20,3 +20,20 @@ data class RegisterRequest(
     val password: String,
     val username: String
 )
+
+@JsonClass(generateAdapter = true)
+data class RegisterResponse(
+    @field:Json(name = "id") val id: String,
+    @field:Json(name = "username") val username: String,
+    @field:Json(name = "display_name") val displayName: String,
+    @field:Json(name = "reg_email") val regEmail: String,
+    @field:Json(name = "current_email") val currentEmail: String?,
+    @field:Json(name = "avatar_url") val avatarUrl: String?,
+    @field:Json(name = "bio") val bio: String?,
+    @field:Json(name = "is_active") val isActive: Boolean,
+    @field:Json(name = "is_verified") val isVerified: Boolean,
+    @field:Json(name = "status") val status: String,
+    @field:Json(name = "reg_date") val regDate: String,
+    @field:Json(name = "last_login") val lastLogin: String?,
+    @field:Json(name = "updated_at") val updatedAt: String?
+)
