b78016826f554a2fdde783c31a67835fcbf3bfdd
Eric85710 <ericchen87887@gmail.com>
2026-05-16 16:11:18 +0800

    dm-11

--- app/src/main/java/com/example/login_v3/data/repository/dm/DM_Basic_Repository.kt
+++ app/src/main/java/com/example/login_v3/data/repository/dm/DM_Basic_Repository.kt
@@ -1,6 +1,7 @@
 package com.example.login_v3.data.repository.dm
 
 import com.example.login_v3.data.api.TecnologiaApi
+import com.example.login_v3.data.api.api_class.MessageResponse
 import com.example.login_v3.data.api.api_class.RoomListResponse
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -16,6 +17,25 @@ class ChatRoomsRepository @Inject constructor(
                 Result.success(response.body()!!)
             } else {
                 Result.failure(Exception("Error code: ${response.code()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getChatMessages(roomId: String): Result<MessageResponse> {
+        return try {
+            val response = api.getChatMessages(roomId) // 確保 TecnologiaApi 裡有 getChatMessages 方法
+
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body != null) {
+                    Result.success(body)
+                } else {
+                    Result.failure(Exception("回應身體為空 (Empty response body)"))
+                }
+            } else {
+                Result.failure(Exception("網路請求失敗，錯誤碼: ${response.code()}"))
             }
         } catch (e: Exception) {
             Result.failure(e)
---
+++ app/src/main/java/com/example/login_v3/home/Message/ViewModel/Detail/Message_Messaging_ViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.login_v3.home.Message.ViewModel.Detail
+
+class Message_Messaging_ViewModel {
+}
